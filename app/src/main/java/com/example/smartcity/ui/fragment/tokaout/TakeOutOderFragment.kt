9f283f3c7a28111fa0fda6893a86beb12a8e8f81
Sakura <3420757701@qq.com>
2023-11-18 02:48:24 +0800

    外卖未完成

--- app/src/main/java/com/example/smartcity/ui/fragment/tokaout/TakeOutOderFragment.kt
+++ app/src/main/java/com/example/smartcity/ui/fragment/tokaout/TakeOutOderFragment.kt
@@ -1,21 +1,25 @@
 package com.example.smartcity.ui.fragment.tokaout
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.smartcity.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.example.smartcity.*
+import com.example.smartcity.adapter.GenericAdapter
 import com.example.smartcity.bean.OrderAllBean
 import com.example.smartcity.databinding.FragmentTakeOutOderBinding
-import com.example.smartcity.g
-import com.example.smartcity.tool
-import com.example.smartcity.viewBinding
+import com.example.smartcity.databinding.ItemOrderAllListBinding
 import com.google.android.material.tabs.TabLayout
 
 
 class TakeOutOderFragment : Fragment() {
     private val vb by viewBinding(FragmentTakeOutOderBinding::inflate)
+    val TAG = "TakeOutOderFragment"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -71,7 +75,29 @@ class TakeOutOderFragment : Fragment() {
         tool.apply {
             send("/prod-api/api/takeout/order/list","GET",null,true){
                 val data = g.fromJson(it,OrderAllBean::class.java).rows
-
+//                Log.e(TAG, "loadAll: ${data.size}")
+//                val index = data.size
+//                Log.e(TAG, "loadAll: $index", )
+                vb.takeAllList.adapter = GenericAdapter(10,
+                    { ItemOrderAllListBinding.inflate(layoutInflater) }) { binding,position->
+                    Glide.with(context).load(getUrl(data[position].sellerInfo.imgUrl)).transform(RoundedCorners(5.dp)).into(binding.itemOrderAllImgUrl)
+                    binding.itemOrderAllName.text = data[position].sellerInfo.name
+                    binding.itemOrderAllTotalPrice.text = "￥${data[position].orderInfo.orderItemList[0].totalPrice}"
+                    binding.itemOrderAllQuantity.text = "X${data[position].orderInfo.orderItemList[0].quantity}"
+                    when(data[position].orderInfo.status) {
+                        "待支付" -> {
+                            binding.itemOrderAllZhifu.visibility = View.VISIBLE
+                        }
+                        "待评价" -> {
+                            binding.itemOrderAllReview.visibility = View.VISIBLE
+                            binding.itemOrderAllTuikuan.visibility = View.VISIBLE
+                        }
+                        "完成" -> {
+                            binding.itemOrderAllDoitagain.visibility = View.VISIBLE
+                        }
+                    }
+                }
+                vb.takeAllList.layoutManager = LinearLayoutManager(context)
             }
         }
     }
